26b4e17edf93edb8b71894a6f65da016372b3fd3
Sangwoo Byeon <tkddn0518@naver.com>
2023-10-07 14:48:37 +0900

    [Create] new project

---
+++ ByeonSangwoo-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByeonSangwoo-practice
+//
+//  Created by 변상우 on 10/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
