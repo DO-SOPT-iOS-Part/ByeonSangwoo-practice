be1f5ea983787b05b6d191d7585684c759108704
Sangwoo Byeon <tkddn0518@naver.com>
2023-10-13 02:34:31 +0900

    [Feat] 1주차 세미나 - 스토리보드릍 통한 UI 뷰 구현, 델리게이트, 클로저 등을 이용한 데이터 값 넘기기 실습

---
+++ ByeonSangwoo-practice/GetDataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetDataProtocol.swift
+//  ByeonSangwoo-practice
+//
+//  Created by 변상우 on 10/7/23.
+//
+
+import Foundation
+
+protocol GetDataProtocol {
+    func getLoginData(email: String, password: String)
+}
